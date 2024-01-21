d21d733525218c28bee21c32a1f98bd24e44306a
Keith L <keithlehman@proton.me>
2024-01-21 01:48:03 -0500

    Main layout built

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { AboutComponent } from './pages/about/about.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { EducationComponent } from './pages/education/education.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ExperienceComponent,
+    EducationComponent,
+    ProjectsComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
