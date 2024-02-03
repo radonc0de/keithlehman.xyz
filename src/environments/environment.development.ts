6929919c5a9a00e6fdd8c675ba441d808bd233ec
Keith L <keithlehman@proton.me>
2024-02-03 11:32:41 -0500

    Updated links, removed Collage project

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+	resumeLink: "https://drive.proton.me/urls/3Q6DKDZJYG#wBqAPub3A5X3",
+	linkedinLink: "https://www.linkedin.com/in/keith-lehman-930949224/",
+};
